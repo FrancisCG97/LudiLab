479c5dbafd3630e086eb9d16aa563afe23098ce7
Francis <f.carrasco.g.07@gmail.com>
2024-07-10 11:20:54 -0400

    Setup ready

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,23 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(
+      {
+        "projectId": "fir-crud-df0f5",
+        "appId": "1:845834747164:web:195b4ad9a1b4235baa4ed9",
+        "storageBucket": "fir-crud-df0f5.appspot.com",
+        "authDomain": "fir-crud-df0f5.firebaseapp.com",
+        "messagingSenderId": "845834747164"
+      }
+    )), provideFirestore(() => getFirestore())
+  ]
 };
